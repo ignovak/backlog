7a13cb515e4e1546b47d08dcd40f2fb6700c0e88
Igor <bezengi@gmail.com>
2011-07-11 05:57:42 +0300

    edit sh script

--- download_data.sh
+++ download_data.sh
@@ -1,1 +1,1 @@
-appcfg.py upload_data --config_file=bulkloader.yaml --filename=backlog.csv --kind=BacklogItem --url=http://localhost:8081/_ah/remote_api /home/user/Desktop/google_appengine/demos/backlog
+appcfg.py download_data --config_file=backlog/bulkloader.yaml --filename=backlog/backlog.csv --kind=BacklogItem --url=http://localhost:8081/_ah/remote_api backlog/
